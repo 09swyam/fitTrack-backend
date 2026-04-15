e3a6fad72079f89ed566f3a8c2bf70190270b611
09swyam <09swyam@gmail.com>
2026-04-15 12:42:39 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -1,42 +1,48 @@
 require("dotenv").config({ path: "./.env" });
-const express = require('express');
-const connectDB = require('./config/connectDB');
-const cookieParser = require('cookie-parser');
-const authRouter = require('./routes/auth');
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const workoutRouter = require("./routes/workout");
 const exerciseRouter = require("./routes/exercise");
 const caloriesRouter = require("./routes/calories");
-const cors = require('cors');
+const cors = require("cors");
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fit-track-frontend-twmg.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.use('/', authRouter);
+// health route (important for Render)
+app.get("/", (req, res) => {
+  res.send("FitTrack API running");
+});
 
-app.use('/', profileRouter);
-
-app.use('/', workoutRouter);
-
-app.use('/', exerciseRouter);
-
-app.use('/', caloriesRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", workoutRouter);
+app.use("/", exerciseRouter);
+app.use("/", caloriesRouter);
 
 connectDB()
-.then(() => {
+  .then(() => {
     console.log("Connected to DB successfully");
-    app.listen(process.env.PORT, () => {
-        console.log("Server is running on port 3000");
-    })
-})
-.catch((err) => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("Error connecting to DB: ", err.message);
-})
+  });
